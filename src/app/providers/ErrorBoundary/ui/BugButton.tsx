ab8a34cbc566e24f82ac80df5786dc255397b3e9
MJafar777 <mirzaraximovjafar2001@gmail.com>
2023-10-19 17:42:55 +0500

    22 dars / error boundary yasash

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/ui/Button/Button';
+
+export const BugButton = () => {
+    const [error, setError] = useState(false);
+    const { t } = useTranslation();
+
+    const onThrow = () => setError(true);
+
+    useEffect(() => {
+        if (error) {
+            throw new Error();
+        }
+    }, [error]);
+
+    return (
+        <Button
+            onClick={onThrow}
+        >
+            {t('throw error')}
+        </Button>
+    );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,15 @@ import "./shared/config/i18n/i18n";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "app/providers/ThemeProvider";
 
+import { ErrorBoundary } from './app/providers/ErrorBoundary';
+
 ReactDOM.render(
   <BrowserRouter>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <ErrorBoundary>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </ErrorBoundary>
   </BrowserRouter>,
   document.getElementById("root") as HTMLElement
 );
