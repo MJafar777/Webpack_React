a211cc494e056f0f1f684837871ea07bc096bfd5
MJafar777 <mirzaraximovjafar2001@gmail.com>
2023-11-01 13:29:22 +0500

    Fix bugs

--- src/app/providers/StoreProvider/index.ts
+++ src/app/providers/StoreProvider/index.ts
@@ -1,6 +1,6 @@
 import { StoreProvider } from './ui/StoreProvider';
-import { createReduxStore, AppDispatch } from './config/store';
-import type { StateSchema, ReduxStoreWithManager, ThunkConfig } from './config/StateSchema';
+import { createReduxStore, AppDispatch } from './Config/store';
+import type { StateSchema, ReduxStoreWithManager, ThunkConfig } from './Config/StateSchema';
 
 export {
     StoreProvider,
