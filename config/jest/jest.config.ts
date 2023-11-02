af59ad45f443c28fc40bd24d042c35b18c4226a4
MJafar777 <mirzaraximovjafar2001@gmail.com>
2023-11-02 11:08:09 +0500

    71 dars tugadi

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -34,5 +34,13 @@ export default {
     moduleNameMapper: {
         '\\.s?css$': 'identity-obj-proxy',
         '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
-    },
+    },reporters: [
+        'default',
+        ['jest-html-reporters', {
+            publicPath: '<rootDir>/reports/unit',
+            filename: 'report.html',
+            openReport: true,
+            inlineSource: true,
+        }],
+    ],
 };
