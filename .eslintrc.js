ee381c9bcb8d99e982e894fe64e6e2df479a9894
MJafar777 <mirzaraximovjafar2001@gmail.com>
2023-11-06 11:49:00 +0500

    testing eslint exports

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,83 @@
-"use strict";
-
 module.exports = {
-  root: true,
-  extends: [
-    "eslint:recommended",
-    "plugin:eslint-plugin/recommended",
-    "plugin:node/recommended",
-  ],
   env: {
-    node: true,
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:i18next/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'i18next', 'react-hooks', 'ulbi-tv-plugin', 'unused-imports'],
+  rules: {
+    'react/jsx-no-undef': 'off',
+    'react/jsx-indent': ['error', 4],
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': 'off',
+    'react/require-default-props': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'warn',
+    'react/function-component-definition': 'off',
+    'no-shadow': 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-underscore-dangle': 'off',
+    'i18next/no-literal-string': [
+      'error',
+      {
+        markupOnly: true,
+        ignoreAttribute: ['as', 'role', 'data-testid', 'to', 'target', 'justify', 'align', 'border', 'direction', 'gap'],
+      },
+    ],
+    'max-len': ['error', { ignoreComments: true, code: 125 }],
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'error', // Checks effect dependencies,
+    'no-param-reassign': 'off',
+    'no-undef': 'off',
+    'react/no-array-index-key': 'off',
+    'arrow-body-style': 'off',
+    'ulbi-tv-plugin/path-checker': ['error', { alias: '@' }],
+    'ulbi-tv-plugin/layer-imports': [
+      'error',
+      {
+        alias: '@',
+        ignoreImportPatterns: ['**/StoreProvider', '**/testing'],
+      },
+    ],
+    'ulbi-tv-plugin/public-api-imports': [
+      'error',
+      {
+        alias: '@',
+        testFilesPatterns: ['**/*.test.*', '**/*.story.*', '**/StoreDecorator.tsx'],
+      },
+    ],
+    'react/jsx-indent-props': 'off',
+    'eol-last': 'off',
+    'react/jsx-closing-bracket-location': 'off',
+  },
+  globals: {
+    __IS_DEV__: true,
+    __API__: true,
+    __PROJECT__: true,
   },
   overrides: [
     {
-      files: ["tests/**/*.js"],
-      env: { mocha: true },
+      files: ['**/src/**/*.{test,stories}.{ts,tsx}', '*.cy.ts*'],
+      rules: {
+        'i18next/no-literal-string': 'off',
+        'max-len': 'off',
+        'react/jsx-indent': 'off',
+        'react/jsx-no-undef': 'off',
+      },
     },
   ],
 };
--- src/features/AuthByUsername/index.ts
+++ src/features/AuthByUsername/index.ts
@@ -1,2 +1,3 @@
 export { LoginModal } from './ui/LoginModal/LoginModal';
+
 export type { LoginSchema } from './model/types/loginSchema';
