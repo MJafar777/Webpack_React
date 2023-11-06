e15b9d0eecafca6ab6db5f5b6f121d6a419576ec
MJafar777 <mirzaraximovjafar2001@gmail.com>
2023-11-06 11:22:56 +0500

    created new library for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,82 +1,19 @@
+"use strict";
+
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    jest: true,
-  },
+  root: true,
   extends: [
     "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:storybook/recommended",
-    "plugin:@typescript-eslint/recommended",
+    "plugin:eslint-plugin/recommended",
+    "plugin:node/recommended",
   ],
+  env: {
+    node: true,
+  },
   overrides: [
     {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
-    },
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["react", "@typescript-eslint", "i18next"],
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
-  rules: {
-    // "react/jsx-indent": [2, 2],
-    // "react/jsx-indent-props": [2, 2],
-    // indent: [2, 2], // eslint line qatorlarini tree ko'rinishiga keltiradi
-    "react/jsx-filename-extension": [
-      1,
-      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
-    ],
-    "import/no-unresolved": "off",
-    "import/prefer-default-export": "off",
-    "no-unused-vars": "warn",
-    "react/require-default-props": "off",
-    "react/jsx-props-no-spreading": "warn",
-    "react/function-component-definition": "off",
-    "no-shadow": "off",
-    "no-underscore-dangle": "off",
-    "i18next/no-literal-string": [
-      "error",
-      {
-        markupOnly: true,
-        ignoreAttribute: [
-          "as",
-          "role",
-          "data-testId",
-          "to",
-          "target",
-          "justify",
-          "align",
-          "direction",
-          "gap",
-          "border",
-        ],
-      },
-    ],
-  },
-  "max-len": ["error", { ignoreComments: true, code: 125 }],
-  // global :{
-  //   '__IS_DEV__': true
-  // }
-  overides: [
-    {
-      files: ["**/src/**/*.test.{ts,tsx}"],
-      rules: {
-        "i18next/no-literal-string": "off",
-      },
+      files: ["tests/**/*.js"],
+      env: { mocha: true },
     },
   ],
 };
