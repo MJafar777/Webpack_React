0a2be45a5f6e9dd3a2ed9275499cb6e6981922a6
MJafar777 <mirzaraximovjafar2001@gmail.com>
2023-11-06 12:14:54 +0500

    refactoring router configs

--- src/shared/const/router.ts
+++ src/shared/const/router.ts
@@ -1,23 +1,22 @@
 export enum AppRoutes {
-    MAIN = 'main',
-    ABOUT = 'about',
-    PROFILE = 'profile',
-    ARTICLES = 'articles',
-    ARTICLE_DETAILS = 'article_details',
-    ARTICLE_CREATE = 'article_create',
-    ARTICLE_EDIT = 'article_edit',
-    ADMIN_PANEL = 'admin_panel',
-    FORBIDDEN = 'forbidden',
-    // last
-    NOT_FOUND = 'not_found',
+  MAIN = 'main',
+  ABOUT = 'about',
+  PROFILE = 'profile',
+  ARTICLES = 'articles',
+  ARTICLE_DETAILS = 'article_details',
+  ARTICLE_CREATE = 'article_create',
+  ARTICLE_EDIT = 'article_edit',
+  ADMIN_PANEL = 'admin_panel',
+  FORBIDDEN = 'forbidden',
+  NOT_FOUND = 'not_found',
 }
 
 export const getRouteMain = () => '/';
 export const getRouteAbout = () => '/about';
-export const getRouteProfile = (id: string) => `/profile/${id}`;
+export const getRouteAdmin = () => '/admin';
 export const getRouteArticles = () => '/articles';
-export const getRouteArticleDetails = (id: string) => `/articles/${id}`;
+export const getRouteForbidden = () => '/forbidden';
 export const getRouteArticleCreate = () => '/articles/new';
 export const getRouteArticleEdit = (id: string) => `/articles/${id}/edit`;
-export const getRouteAdmin = () => '/admin';
-export const getRouteForbidden = () => '/forbidden';
+export const getRouteArticleDetails = (id: string) => `/articles/${id}`;
+export const getRouteProfile = (id: string) => `/profile/${id}`;
