28e85dbe63774f3dc747857016372de043c81db9
MJafar777 <mirzaraximovjafar2001@gmail.com>
2023-11-06 16:07:47 +0500

    storybook addon theme

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -1,22 +1,20 @@
 module.exports = {
-    stories: [
-        '../../src/**/*.stories.@(js|jsx|ts|tsx)',
-    ],
-    addons: [
-        // '@storybook/addon-links',
-        // {
-        //     name: '@storybook/addon-essentials',
-        //     options: {
-        //         backgrounds: false,
-        //     },
-        // },
-        // '@storybook/addon-interactions',
-        // 'storybook-addon-mock/register',
-        // 'storybook-addon-themes',
-    ],
-    framework: '@storybook/react',
-    core: {
-        builder: 'webpack5',
-    },
-    typescript: { reactDocgen: false }
+  stories: ['../../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    // '@storybook/addon-links',
+    // {
+    //   name: '@storybook/addon-essentials',
+    //   options: {
+    //     backgrounds: false,
+    //   },
+    // },
+    // '@storybook/addon-interactions',
+    // 'storybook-addon-mock/register',
+    'storybook-addon-themes',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+  },
+  typescript: { reactDocgen: false },
 };
--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -3,27 +3,27 @@ import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
 import {
-    SuspenseDecorator,
+  SuspenseDecorator,
 } from '../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
 import { Theme } from '../../src/app/providers/ThemeProvider/lib/ThemeContext';
 
 export const parameters = {
-    actions: { argTypesRegex: '^on[A-Z].*' },
-    controls: {
-        matchers: {
-            color: /(background|color)$/i,
-            date: /Date$/,
-        },
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
     },
-    layout: 'fullscreen',
-    themes: {
-        default: 'light',
-        list: [
-            { name: 'light', class: Theme.LIGHT, color: '#ffffff' },
-            { name: 'dark', class: Theme.DARK, color: '#000000' },
-            { name: 'orange', class: Theme.ORANGE, color: '#ffb005' },
-        ],
-    },
+  },
+  layout: 'fullscreen',
+  themes: {
+    default: 'light',
+    list: [
+      { name: 'light', class: Theme.LIGHT, color: '#ffffff' },
+      { name: 'dark', class: Theme.DARK, color: '#000000' },
+      { name: 'orange', class: Theme.ORANGE, color: '#ffb005' },
+    ],
+  },
 };
 
 addDecorator(StyleDecorator);
